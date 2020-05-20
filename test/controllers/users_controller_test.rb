d28a0494def2368fd39af602b3d37861ac9690a4
masahiro331 <mur4m4s4.331@gmail.com>
2020-05-20 14:02:26 +0900

    first commit

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get users_url, as: :json
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { new: @user.new } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show user" do
+    get user_url(@user), as: :json
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { user: { new: @user.new } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user), as: :json
+    end
+
+    assert_response 204
+  end
+end
